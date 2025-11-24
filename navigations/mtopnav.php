b281c4dd165becea6dfa72a9bb3590f2e7381f58
scripted001 <157211493+scripted001@users.noreply.github.com>
2025-11-24 10:05:10 +0100

    Add files via upload

---
+++ navigations/mtopnav.php
@@ -0,0 +1,28 @@
+<?php
+
+echo '
+    <session class="mtopBar">
+            
+            <span class="mlbar">
+                <!-- <a href="#" class="mtopmenu">Marketplace</a> -->
+                <a href="#" class="mtopmenu">My Network</a>
+                <a href="index.php" class="mtopmenu">For You</a>
+            </span>
+            <span class="msbar">
+               <!-- <span class="mhighlight">
+                    200
+                </span> -->
+                <button class="mbtn-hide" onclick="menu()"><i class="fa fa-bars fa-1x"></i></button>
+            </span>
+         </session>
+';
+echo '
+   <!-- ORISAVERSE SIDEBAR -->
+<div id="menu">
+  
+</div>
+
+';
+
+
+?>
