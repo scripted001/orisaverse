449755141601dbf4211b28de7f21c7896b570be6
scripted001 <157211493+scripted001@users.noreply.github.com>
2026-01-02 19:17:10 +0100

    Update postcardScroll.js

--- programFiles/plugins/postcardScroll.js
+++ programFiles/plugins/postcardScroll.js
@@ -1,14 +1,1 @@
-// programFiles/plugins/postcardScroll.js
-document.addEventListener("DOMContentLoaded", () => {
-    const cards = document.querySelectorAll(".postcard");
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if(entry.isIntersecting){
-                entry.target.style.animation = "fadeIn 2s forwards";
-            }
-        });
-    }, { threshold: 0.6 }); // trigger when 60% visible
-
-    cards.forEach(card => observer.observe(card));
-});
